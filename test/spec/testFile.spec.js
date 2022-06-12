b869a72dac7eb4d90b61e8759bc2a3ab80818b41
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2022-06-12 01:00:30 -0300

    Create testFile.spec.js

---
+++ test/spec/testFile.spec.js
@@ -0,0 +1,8 @@
+const test = require('../eval/fileToEvaluate');
+
+describe('functionName', () => {
+  test('describe what the function does', () => {
+    const varName = "";
+    expect(functionName(varName)).toBe('');
+  });
+});
